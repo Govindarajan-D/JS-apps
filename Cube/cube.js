3942127d8191ab2c7a87e747f2d7902ca4183b1b
Govindarajan Devarajan <govin1993@hotmail.com>
2015-01-15 10:20:46 +0530

    3D Rotating Cube
    Creates a 3D Rotating cube with letters from the string passed.

---
+++ Cube/cube.js
@@ -0,0 +1,31 @@
+function cube(text){
+	var cubeContainer = document.getElementById("cube-container");
+	var initial = 20, offset = 0; //Alter these to change position
+	var cubeString = text;
+	var textArray = cubeString.toUpperCase().split('');
+	var sideNames = ["one", "two", "three", "four", "five", "six"];
+
+	for(i=0;i<textArray.length;i++){
+		var cubeNode = document.createElement("div");
+		cubeNode.id = "cube" + (i+1);
+		if(i%2)
+		cubeNode.className = "cubeodd";
+		else
+		cubeNode.className = "cubeeven";
+		
+		var posTop = parseInt(initial,10) + parseInt(offset,10);
+		var cubeStyle = "position: absolute; left:40px; top:" + posTop + "px";
+		offset += 100; 
+		
+		cubeContainer.appendChild(cubeNode);
+		cubeNode.setAttribute("style",cubeStyle);
+		
+		for(j=0; j<6;j++){
+			var sideNode = document.createElement("div");
+			var cubeText = document.createTextNode(textArray[i]);
+			sideNode.className ="side " + sideNames[j];
+			cubeNode.appendChild(sideNode);
+			sideNode.appendChild(cubeText);
+		}
+	}
+}
